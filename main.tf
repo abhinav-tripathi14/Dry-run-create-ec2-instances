854a4f16ed7bd8d0dba53ff6a375cd47b093ea4b
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2026-07-15 22:49:06 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform { 
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.89.0"
+    }
+  }
+   cloud {
+   organization = "Test-Abhinav"
+   workspaces {
+      name = "prod-ec2-instances"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "east2"
+  region = "us-east-2"
+}
+
+module "create-ec2" {
+  source     = "./modules/create-ec2"
+  instance_type = "t3.micro"
+  providers = {
+    #aws.east2 = aws.east2
+    aws = aws.east2
+  }
+}
