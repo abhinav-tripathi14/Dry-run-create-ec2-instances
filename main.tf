01faa48e6e95c311d229c10c497901ae07af82d4
abhinav-tripathi14 <abhi.tripathi14@gmail.com>
2026-07-16 00:04:49 +0530

    updated files

--- main.tf
+++ main.tf
@@ -13,11 +13,35 @@ terraform {
   }
 }
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "east2"
+  region = "us-east-2"
+}
 
 module "create-ec2" {
   source     = "./modules/create-ec2"
   instance_type = "t3.micro"
   providers = {
-    aws = aws.east2
+    aws.east2 = aws.east2
+  }
+}
+
+variable "resources_to_create" {
+  default = {
+    count =2
+  }
+}
+
+resource "aws_instance" "root_instances" {
+  count = var.resources_to_create.count
+  ami           = "ami-0e5497a77ef21b5ac"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "root-instance-${count.index}"
   }
 }
